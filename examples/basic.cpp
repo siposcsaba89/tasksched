e2dd865f3a1884fa10f460c8eef8ffce5723d635
Csaba Sipos <siposcsaba89@gmail.com>
2017-12-03 17:00:25 +0100

    first commit, files added

---
+++ examples/basic.cpp
@@ -0,0 +1,86 @@
+#include "tasksched/tasksched.h"
+#include <string>
+#include <iostream>
+
+
+
+class SampleTask : public task
+{
+public:
+    SampleTask(const std::string & name, int sleep_time) { m_name = name; m_sleep_time = sleep_time; }
+    virtual void execute() override
+    {
+        std::cout << m_name << ": " << m_task_id << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(m_sleep_time));
+    }
+    std::string m_name;
+    int m_sleep_time;
+};
+
+
+int32_t threadsched::s_task_id = 0;
+
+
+
+int main()
+{
+    SampleTask T1("T1", 10), T2("T2", 10), T3("T3", 20), T4("T4", 30), T5("T5", 35), T6("T6", 3), T7("T7", 90), T8("T8", 16), T9("T9", 25), T10("T10", 1), T11("T11", 46), T12("T12", 101), T13("T13", 32), T14("T14", 32);
+    T1.set_priority(10);
+    T2.set_priority(10);
+    T3.set_priority(20);
+    T4.set_priority(30);
+    T5.set_priority(35);
+    T6.set_priority(3);
+    T7.set_priority(90);
+    T8.set_priority(16);
+    T9.set_priority(25);
+    T10.set_priority(1);
+    T11.set_priority(46);
+    T12.set_priority(101);
+    T13.set_priority(32);
+    T14.set_priority(32);
+
+    threadsched sched(6);
+    sched.add_task(&T1);
+    sched.add_task(&T2);
+    sched.add_task(&T3);
+    sched.add_task(&T4);
+    sched.add_task(&T5);
+    sched.add_task(&T6);
+    sched.add_task(&T7);
+    sched.add_task(&T8);
+    sched.add_task(&T9);
+    sched.add_task(&T10);
+    sched.add_task(&T11);
+    sched.add_task(&T12);
+    sched.add_task(&T13);
+    sched.add_task(&T14);
+
+    sched.add_dependency(&T1, &T2);
+    sched.add_dependency(&T1, &T3);
+    sched.add_dependency(&T2, &T5);
+    sched.add_dependency(&T2, &T4);
+    sched.add_dependency(&T3, &T4);
+    sched.add_dependency(&T5, &T6);
+    sched.add_dependency(&T4, &T6);
+    sched.add_dependency(&T1, &T8);
+    sched.add_dependency(&T8, &T9);
+    sched.add_dependency(&T8, &T10);
+    sched.add_dependency(&T9, &T11);
+    sched.add_dependency(&T10, &T11);
+    sched.add_dependency(&T1, &T7);
+    sched.add_dependency(&T13, &T14);
+
+
+    sched.start();
+
+    for (int i = 0; i < 100; ++i)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        //std::cout << "Meg futok: " << i << std::endl;
+    }
+    sched.finish();
+    std::cout << "Lealltam" << std::endl;
+
+    return 0;
+}
---
+++ include/tasksched/tasksched.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <inttypes.h>
+#include <mutex>
+#include <memory>
+#include <thread>
+#include <vector>
+#include <cassert>
+#include <queue>
+#include <map>
+
+struct iomanager
+{
+
+};
+
+class task
+{
+public:
+    virtual void execute() = 0;
+    virtual void set_iomanager(iomanager * manager) { m_manager = manager; }
+    void set_priority(int32_t priority) { m_priority = priority; }
+    int32_t priority() { return m_priority; }
+private:
+    friend class threadsched;
+    void set_task_id(int32_t t_id) { m_task_id = t_id; }
+    int32_t task_id() { assert(m_task_id != -1); return m_task_id; };
+protected:
+    iomanager * m_manager = nullptr;
+    int32_t m_task_id = -1;
+    int32_t m_priority = -1;
+};
+
+
+class threadsched
+{
+public:
+    threadsched(int32_t num_threads);
+    void start();
+    void finish();
+    void add_task(task * t);
+    void add_dependency(task *t1, task* t2);
+    void add_dependency_by_id(int32_t t1_id, int32_t t2_id);
+private:
+    void update_queue(task * finsihed_task);
+    void clear_task_executed();
+    bool all_task_executed() const;
+private:
+    std::vector<std::thread> m_workers;
+    std::vector<task *> m_tasks;
+    std::map<int32_t, std::vector<int32_t>> m_deps_ready;
+    std::map<int32_t, std::vector<int32_t> > m_forward_deps;
+    std::map<int32_t, std::vector<int32_t> > m_backward_deps;
+    //std::
+    static int32_t s_task_id;
+    std::mutex m_task_queue_mutex;
+    struct compare_tasks
+    {
+        bool operator()(task * t1, task * t2) { return t1->priority() < t2->priority(); }
+    };
+    std::priority_queue < task*, std::vector<task*>, compare_tasks > m_task_queue;
+    bool m_stop_execution = false;
+
+    std::mutex m_sched_mutex;
+    std::map<int32_t, bool> m_task_executed;
+    std::map<int32_t, bool> m_task_scheduled;
+
+};
+
