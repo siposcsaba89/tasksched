09d316e2d1441fb83142fc2a4a7d0d282211c961
Csaba Sipos <csaba.sipos@aimotive.com>
2018-05-31 11:21:55 +0200

    fixing doc comments

--- include/tasksched/timeline_creator.h
+++ include/tasksched/timeline_creator.h
@@ -4,18 +4,16 @@
 #include <map>
 #include <list>
 
-/*! \brief Manages timeline? creation for what? task scheduling?
- */
 namespace tsch
 {
     struct TimePointData
     {
         std::string group;
-        std::chrono::time_point<std::chrono::steady_clock> start;                       /*!< Starting time point of what? */.
-        std::chrono::time_point<std::chrono::steady_clock> end;                         /*!< End time point of what? */.
+        std::chrono::time_point<std::chrono::steady_clock> start;
+        std::chrono::time_point<std::chrono::steady_clock> end;
     };
 
-    void createTimelineHTML(const std::string & f_name, const std::string & header,     /*!< Creates timeline of what? in HTML */.
-        std::chrono::time_point<std::chrono::steady_clock> start_time,                  /*!< Starting time */.
-        const std::map<std::string, std::list<tsch::TimePointData>> & data);            /*!< Assigns map data to time points */.
+    void createTimelineHTML(const std::string & f_name, const std::string & header,
+        std::chrono::time_point<std::chrono::steady_clock> start_time,
+        const std::map<std::string, std::list<tsch::TimePointData>> & data);
 }
